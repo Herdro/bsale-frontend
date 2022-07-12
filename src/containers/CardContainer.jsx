e3db490e5ecc4bea4b432aa65f2ac69a7dcad0d1
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-07-11 21:56:00 -0400

    style cardContainer

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect } from "react";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import ProductCard from "@components/ProductCard";
 import useGetProducts from "../hooks/useGetProducts";
 import "../styles/Cards.scss";
 
 const CardContainer = () => {
-  let [searchParams, setSearchParams] = useSearchParams();
-  const entries = Object.fromEntries([...searchParams]);
-  console.log(entries);
-  if (!entries.limit) {
-    setSearchParams({ ...entries ,limit: 10 })
-  }
+  let [searchParams] = useSearchParams();
+  if (searchParams.has('limit') === false) {
+    searchParams.append('limit', '10')
+  } 
 
-  const { search } = useLocation();
-  useEffect (() => {
-  },[search])
-
-  const API = `https://polar-falls-16849.herokuapp.com/api/product${search}`;
+  const API = `https://polar-falls-16849.herokuapp.com/api/product?${searchParams.toString()}`;
   console.log(API);
   const products = useGetProducts(API);
   
