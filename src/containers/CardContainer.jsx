64825e9d3dc3e1200bf09561b7f43965dd6c7ae2
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-07-11 21:35:51 -0400

    add query params in cardContainer

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
 import ProductCard from "@components/ProductCard";
 import useGetProducts from "../hooks/useGetProducts";
 import "../styles/Cards.scss";
 
-const API = "https://polar-falls-16849.herokuapp.com/api/product";
-
 const CardContainer = () => {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const entries = Object.fromEntries([...searchParams]);
+  console.log(entries);
+  if (!entries.limit) {
+    setSearchParams({ ...entries ,limit: 10 })
+  }
+
+  const { search } = useLocation();
+  useEffect (() => {
+  },[search])
+
+  const API = `https://polar-falls-16849.herokuapp.com/api/product${search}`;
+  console.log(API);
   const products = useGetProducts(API);
   
   return (
