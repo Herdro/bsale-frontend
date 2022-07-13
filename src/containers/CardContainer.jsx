be944e16d82c0f4737f3978858214584b693760f
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-07-13 15:47:31 -0400

    add search bar

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useRef } from "react";
+import { useSearchParams, useNavigate, } from "react-router-dom";
 import ProductCard from "@components/ProductCard";
 import useGetProducts from "../hooks/useGetProducts";
 import "../styles/Cards.scss";
 
 const CardContainer = () => {
   let [searchParams] = useSearchParams();
-  if (searchParams.has('limit') === false) {
-    searchParams.append('limit', '10')
-  } 
+  if (searchParams.has("limit") === false) {
+    searchParams.append("limit", "10");
+  }
+
+  const form = useRef(null);
+
+  const handleSubmit = () => {
+    const formData = new FormData(form.current);
+    const data = {
+      search: formData.get("search"),
+    };
+		console.log(data);
+  };
 
   const API = `https://polar-falls-16849.herokuapp.com/api/product?${searchParams.toString()}`;
   console.log(API);
   const products = useGetProducts(API);
-  
+
   return (
     <section className="main-container">
+      <form action="/" className="form" ref={form}>
+        <input
+          type="text"
+          name="search"
+          placeholder="busqueda"
+          className="input input-email"
+        />
+        <button onClick={handleSubmit} className="primary-button login-button">
+          Buscar
+        </button>
+      </form>
       <div className="cards-container">
-        {products.map(product => (
-            <ProductCard product={product} key={product.id} />
+        {products.map((product) => (
+          <ProductCard product={product} key={product.id} />
         ))}
       </div>
     </section>
